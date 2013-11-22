befaad66d2d07ecc44e3c16dfd9b81d9c9f8d0a2
prabhakar <undurthi.prabhakar@gmail.com>
2013-11-22 13:35:23 +0530

    Created the rails app

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'haml'
+gem 'haml-rails'
+gem 'sass'
+gem "jquery-rails"
+gem "compass", ">= 0.11.3"
+
+gem 'sqlite3'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyjobs::Application.config.secret_token = '61832dbd22d7531b4a4549cdedccbd5c6fded58505dd5bee855b1df459ecc9ba7b00025e84421d74315153767db1b1daac393e2c3c64aba2965b8c68d286aa4a'
